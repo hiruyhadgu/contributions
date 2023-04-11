c75b79d84c2ebc8876a871e29ea5acfcc25a173e
hiruyhadgu <65324470+hiruyhadgu@users.noreply.github.com>
2023-04-11 10:41:06 -0400

    Changes to be committed: 	modified:   pages/3_Figures.py
    Changes not staged for commit:
    	modified:   .DS_Store
    	deleted:    contributions.db
    	modified:   modules/__pycache__/db_load.cpython-39.pyc
    	modified:   modules/__pycache__/functions.cpython-39.pyc
    	modified:   modules/db_load.py
    	modified:   modules/functions.py
    	modified:   pages/1_Overview.py
    	modified:   pages/2_Analysis.py
    
    Untracked files:
    	.ignore/ContributionsList18.xls
    	.ignore/ContributionsList19.xls
    	.ignore/ContributionsList20.xls
    	.ignore/ContributionsList21.xls
    	.ignore/ContributionsList22.xls
    	.ignore/ContributionsList23.xls
    	.ignore/ContributionsList24.xls
    	.ignore/ContributionsList25.xls
    	.ignore/ContributionsList26.xls
    	Untitled.ipynb
    	pages/.DS_Store
    	pages/contributions.db

--- pages/3_Figures.py
+++ pages/3_Figures.py
@@ -2,13 +2,22 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from modules.functions import group_by_filing_period
-from modules.db_load import load, developer
+from modules.db_load import Database, Tables, Table
 import datetime as dt
 import re
 
 ## Load all tables
-raw_table = load()
-raw_table['Contribution Date'] = pd.to_datetime(raw_table['Contribution Date'])
+db = Database('pages/contributions.db')
+tables = Tables(db).fetch_list()
+unpacked_tables = [t[0] for t in tables]
+
+table = Table(unpacked_tables, db)
+raw_table = table.fetch_all()
+
+table1 = Table('developercrossreference', db)
+developer_list = table1.fetch_reference()
+
+raw_table['ContributionDate'] = pd.to_datetime(raw_table['ContributionDate'])
 
 def atoi(text):
     return int(text) if text.isdigit() else text
@@ -21,36 +30,32 @@ def natural_keys(text):
     '''
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
-developer_filter = developer()
-
-developer_list = developer()
-
-developer_verify = developer_list['Developer/Developer Affiliated'].isin(['yes','Yes', 'YES'])
+developer_verify = developer_list['DeveloperOrDeveloperAffiliated'].isin(['yes','Yes', 'YES'])
 developer_list = developer_list[developer_verify]
-developer_mask = raw_table['Contributor Name'].isin(developer_list['Contributor Name'])
+developer_mask = raw_table['ContributorName'].isin(developer_list['ContributorName'])
 
 st.markdown('##### Click on the individual districts to drill down on candidates')
 office = st.selectbox('Select Office Type (CC or CE):', raw_table['Office'].unique())
 @st.cache_data
 def plot1(office):
     mask1 = raw_table['Office']== office
-    fig = px.sunburst(raw_table[mask1], path=['Councilmanic District', 'Candidate Name'], values='Contribution Amount')
+    fig = px.sunburst(raw_table[mask1], path=['CouncilmanicDistrict', 'CandidateName'], values='ContributionAmount')
     st.write(fig)
     st.markdown('----')
-    if office == 'County Executive':
-        x_val = 'Candidate Name'
+    if office == 'CountyExecutive':
+        x_val = 'CandidateName'
     else:
-        x_val = 'Councilmanic District'
-    fig1 = px.bar(raw_table[mask1], x=x_val, y="Contribution Amount", color="Candidate Name", pattern_shape="Candidate Name")
+        x_val = 'CouncilmanicDistrict'
+    fig1 = px.bar(raw_table[mask1], x=x_val, y="ContributionAmount", color="CandidateName", pattern_shape="CandidateName")
     st.write(fig1)
 
 @st.cache_data
 def plot2(value, office, candidate):
-    mask = (raw_table['Office']== office) & (raw_table['Candidate Name']==candidate)
-    slider_mask =  (raw_table["Contribution Amount"] >=value[0]) & (raw_table["Contribution Amount"] <=value[1])
+    mask = (raw_table['Office']== office) & (raw_table['CandidateName']==candidate)
+    slider_mask =  (raw_table["ContributionAmount"] >=value[0]) & (raw_table["ContributionAmount"] <=value[1])
     to_plot = raw_table[mask & slider_mask]
-    count = to_plot['Contribution Amount'].count()
-    fig2 = px.histogram(to_plot, x="Contribution Amount", nbins=100, title = f'Contributions for {candidate} in selected range: {count}')
+    count = to_plot['ContributionAmount'].count()
+    fig2 = px.histogram(to_plot, x="ContributionAmount", nbins=100, title = f'Contributions for {candidate} in selected range: {count}')
     st.write(fig2)
 
 plot1(office)
@@ -58,8 +63,8 @@ plot1(office)
 st.markdown('----')
 st.markdown("### To examine small-dollar contributions, select from the following range.")
 sub_table = raw_table[raw_table['Office']==office]
-candidate_name1 =  sub_table['Candidate Name'].unique()
-candidate_selection1 = st.selectbox('Candidate Name:',
+candidate_name1 =  sub_table['CandidateName'].unique()
+candidate_selection1 = st.selectbox('CandidateName:',
                                     candidate_name1, key = 1)
 slider_range = st.slider("Select Dollar Range ($)", value = [0, 250])
 
@@ -71,13 +76,13 @@ plot_data = st.sidebar.checkbox('Show Plots for Selected Criteria', value=False)
 developer_analysis = st.sidebar.checkbox('Show Developer Contributions')
 st.markdown('### Click on the checkboxes on the sidebar to review developer contribution.')
 # Filter by filing period
-candidate_name =  raw_table['Candidate Name'].unique()
-candidate_selection = st.selectbox('Candidate Name:',
+candidate_name =  raw_table['CandidateName'].unique()
+candidate_selection = st.selectbox('CandidateName:',
                                     candidate_name)
-candidate_mask = raw_table['Candidate Name']==candidate_selection
+candidate_mask = raw_table['CandidateName']==candidate_selection
 filtered_candidate = raw_table[candidate_mask]
 
-filing_options = filtered_candidate['Filing Period'].unique().tolist()
+filing_options = filtered_candidate['FilingPeriod'].unique().tolist()
 filing_options.sort(key=natural_keys)
 filing_options.insert(0,'All Filing Periods')
 select_filing_period = st.selectbox('Select Filing Period',filing_options,0)
@@ -86,7 +91,7 @@ select_filing_period = st.selectbox('Select Filing Period',filing_options,0)
 if select_filing_period =='All Filing Periods':
     filtered_filing_period = filtered_candidate
 else:
-    filing_option_mask = filtered_candidate['Filing Period']==select_filing_period
+    filing_option_mask = filtered_candidate['FilingPeriod']==select_filing_period
     filtered_filing_period = filtered_candidate[filing_option_mask]
 
 
@@ -105,7 +110,7 @@ else:
 
 @st.cache_data
 def grouped_for_plot(df):
-    df_grouped = df.groupby(['Contributor Name', 'Candidate Name']).agg({'Contribution Amount':["sum"], 'Contributor Name':["count"]})
+    df_grouped = df.groupby(['ContributorName', 'CandidateName']).agg({'ContributionAmount':["sum"], 'ContributorName':["count"]})
     df_grouped.columns=['Total Contribution', 'No of Contributions']
     return df_grouped
 
@@ -120,9 +125,9 @@ if plot_data:
             try:
                 # st.write('Select the "Display Developer Contributions" checkbox')
                 data_set=['Developer Donations', 'Remaining']
-                percent_dev = filtered_filing_period[developer_mask]['Contribution Amount'].sum()/filtered_filing_period['Contribution Amount'].sum()
+                percent_dev = filtered_filing_period[developer_mask]['ContributionAmount'].sum()/filtered_filing_period['ContributionAmount'].sum()
                 values = [percent_dev, 1-percent_dev]
-                fig = px.pie(filtered_filing_period['Contribution Amount'], values=values, names=data_set)
+                fig = px.pie(filtered_filing_period['ContributionAmount'], values=values, names=data_set)
                 st.caption('Percent share of contributions from developers. Note that some candidates\
                     participate in public financing or did not file for the selected criteria.')
                 display = st.plotly_chart(fig)
@@ -137,7 +142,7 @@ if plot_data:
                                 max_value = 40)
         try:
             table_cols = grouped_for_plot(to_display).reset_index().sort_values(by=['Total Contribution'], ascending = False).head(n)
-            fig = px.bar(table_cols, x=table_cols['Contributor Name'], y=table_cols['Total Contribution'],color=table_cols['Candidate Name'])
+            fig = px.bar(table_cols, x=table_cols['ContributorName'], y=table_cols['Total Contribution'],color=table_cols['CandidateName'])
             display = st.plotly_chart(fig)
         except ValueError:
             st.markdown('### Data does not exist for selected criteria.')
@@ -149,32 +154,12 @@ if plot_data:
             else:
                 df_master_bar = filtered_candidate
 
-            contribution_by_filing_period = to_display.groupby(['Candidate Name','Filing Period'])
+            contribution_by_filing_period = to_display.groupby(['CandidateName','FilingPeriod'])
             contribution_by_filing_period=contribution_by_filing_period.agg(
-                TotalContribution =('Contribution Amount','sum')).reset_index()
-            fig = px.bar(contribution_by_filing_period, x = contribution_by_filing_period['Filing Period'], 
-                        y=contribution_by_filing_period['TotalContribution'],color=contribution_by_filing_period['Candidate Name'])
+                TotalContribution =('ContributionAmount','sum')).reset_index()
+            fig = px.bar(contribution_by_filing_period, x = contribution_by_filing_period['FilingPeriod'], 
+                        y=contribution_by_filing_period['TotalContribution'],color=contribution_by_filing_period['CandidateName'])
             display = st.plotly_chart(fig)
         except ValueError:
             st.markdown('### Data does not exist for selected criteria.')
             st.code('Try a different filing period.')
-
-# fig4 = px.scatter(raw_table, x="Contribution Amount", y="Candidate Name", color="Councilmanic District",
-#                  title="Countribution Amounts by Candidate and District",
-#                  labels={"Countribution Amount":"Countribution Amount (in thousands)"} # customize axis label
-#                 )
-
-# st.write(fig4)
-# raw_table['Contribution Date'] =  pd.to_datetime(raw_table['Contribution Date'])
-# year = raw_table['Contribution Date'].sort_values().dt.year
-# fig5 = px.scatter(raw_table, x="Filing Period", y="Contribution Amount", animation_frame='Contribution Date',
-#                   animation_group='Contribution Amount', size="Contribution Amount", color="Candidate Name",
-#            hover_name="Candidate Name", size_max=100)
-# st.write(fig5)
-
-
-#histogram
-# hist_table = filtered_by_candidate.value_counts('Contribution Amount').sort_values()
-# print(hist_table)
-# fig = px.histogram(hist_table)
-# st.write(fig)
